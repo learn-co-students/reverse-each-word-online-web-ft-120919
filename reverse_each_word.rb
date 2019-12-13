b601ccae81432a42ed6e7bb347735889f8079729
github username <github email address>
2019-12-12 19:27:05 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 require 'pry'
-def reverse_each_word(string)
-    reversed_words = string.split(" ")
-    new_array = []
-    reversed_words.collect do |reverses_string|
-        new_array << reverses_string.reverse 
-        # binding.pry        
-    end
-    new_array.join(" ")
-end
 
+def reverse_each_word(word)
+    #separate each word with split & turn it into an array
+    #use collect method on the array to iterate over each word
+    #iteration reverses each word
+    #use .join to put the array back into a string
+    word.split.collect do |reverse|
+        reverse.reverse
+    end.join(" ")
+end
